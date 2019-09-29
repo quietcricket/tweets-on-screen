3c0229d5a6d7a6ade00937ab9014af4bd1f53a98
Shang Liang <shang@wewearglasses.com>
2019-09-29 09:04:17 +0800

    no message

--- chrome-extension/iframe.js
+++
@@ -1,7 +0,0 @@
-if (!window.is_top) {
-    chrome.storage.sync.get('tweet', function (data) {
-        console.log(top.location.href, data['tweet']);
-        if (top.document.location.href == data['tweet'])
-            top.get_votes(document.querySelector('.CardContent'));
-    });
-}
