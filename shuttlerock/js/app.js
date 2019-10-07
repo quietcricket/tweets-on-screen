22783c0cf1baf4eeb88af44b7ef2669e67175dc5
Shang Liang <sliang@twitter.com>
2019-10-07 17:16:35 +0800

    Added http request timeout handling Disables update if the internet is too slow to avoid half loaded/broken images Added a field to change scrolling speed in the page

--- shuttlerock/js/app.js
+++ shuttlerock/js/app.js
@@ -1,5 +1,6 @@
 var BOARD_ID = "jkt2019";
-var SPEED = 250 / 60;
+var SPEED = localStorage.getItem("speed") ? parseInt(localStorage.getItem("speed")) : 200;
+$('#speed').val(SPEED);
 var PER_PAGE = 20;
 var url = `https://spotify.api.shuttlerock.com/v2/boards/${BOARD_ID}/entries.json?per_page=${PER_PAGE}`;
 var tweets = [];
@@ -7,6 +8,7 @@ var incommingTweets = [];
 var index = 0;
 var loaded = false;
 var currentId = 'xxx';
+var requestTime = 0;
 
 function genHtml(id, html, name, username, picture, time, image) {
     return `
@@ -28,29 +30,49 @@ function genHtml(id, html, name, username, picture, time, image) {
 
 function fetchData() {
     var page = Math.floor(incommingTweets.length / PER_PAGE) + 1;
+    requestTime = new Date().getTime();
     $.ajax({
-        url: `${url}&page=${page}&random=${(new Date()).getTime()}`,
+        url: `${url}&page=${page}&random=${new Date().getTime()}`,
         dataType: 'json',
     }).then(function (data) {
+        var latency = new Date().getTime() - requestTime;
+        $('#latency').val(latency);
         for (d of data) {
             incommingTweets.push(d);
         }
+
+        if (latency > 2000) {
+            $('#warning').html("NETWORK TOO SLOW,AUTO REFRESHING STOPPED!")
+            return;
+        }
+
         if (data.length == PER_PAGE) {
             fetchData();
         } else {
+
+            setTimeout(fetchData, 5000);
             processIncoming();
         }
+    }, function (error) {
+        $('#latency').val(new Date().getTime() - requestTime);
+        setTimeout(fetchData, 5000);
     });
 }
 
 function processIncoming() {
     var newTweets = [];
+    var newIndex = -1;
     for (var t of incommingTweets) {
-        newTweets.push(t.id);
-        if (tweets.indexOf(t.id) == -1) {
+        newTweets.push(t.short_id);
+        if (tweets.indexOf(t.short_id) == -1) {
             addTweet(t);
-            index = newTweets.length - 1;
+            if (newIndex == -1) {
+                newIndex = newTweets.length - 1;
+            }
         }
+    }
+    if (newIndex != -1) {
+        index = newIndex;
     }
 
     // Remove deleted tweets
@@ -65,6 +87,13 @@ function processIncoming() {
         loaded = true;
         requestAnimationFrame(animate);
     }
+
+    $('.article-img img').each(function () {
+        if (this.height > this.width) {
+            $(this).css('height', $('.l-wrap .article-img img').width());
+        }
+    });
+
     incommingTweets = [];
 }
 
@@ -72,7 +101,7 @@ function addTweet(t) {
     var d = new Date(t.details.created_at);
     var mm = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"][d.getMonth()];
     var time = `${d.getHours()}:${d.getMinutes()} - ${d.getDate()} ${mm} ${d.getFullYear()}`;
-    $('#hidden-items').append(genHtml(t.id, t.details.html_description, t.author.name, t.source.username, t.author.picture, time, t.images.show));
+    $('#hidden-items').append(genHtml(t.short_id, t.details.html_description, t.author.name, t.source.username, t.author.picture, time, t.images.show));
 }
 
 function animate(timestamp) {
@@ -88,7 +117,7 @@ function animate(timestamp) {
     $('#photoArea .article').each(function () {
         var ele = $(this);
         var tx = parseFloat(ele.attr('tx'));
-        tx = Math.round(tx + SPEED);
+        tx = Math.round(tx + SPEED / 60);
         ele.css('transform', `translate3d(0,-${tx}px,0)`);
         ele.attr('tx', tx);
     });
@@ -96,5 +125,11 @@ function animate(timestamp) {
 
 }
 
-setInterval(fetchData, 10000);
+function updateSpeed(ele) {
+    ele.blur();
+    SPEED = parseInt(ele.value);
+    localStorage.setItem("speed", SPEED);
+
+}
+
 fetchData();
